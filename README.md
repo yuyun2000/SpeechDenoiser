5bbb769c75097a83218d8a997859252a2861d3a4
yuyun2000 <15515722313yxw@gmail.com>
2024-07-03 14:41:34 +0800

    v1

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # SpeechDenoiser
 SpeechDenoiser: Real-Time Speech Denoising with ONNX  Welcome to SpeechDenoiser, a simple and effective solution for real-time speech denoising using an ONNX model. This repository contains everything you need to get started with enhancing audio quality by reducing noise, making it perfect for improving voice recordings and live communication.
+
+
+---
+仅支持48kHz的音频；推理代码十分粗糙，但是它可以工作！如果你有经验，你会发现它是一个流式模型，所以可以进行实时降噪！经过我的测试，在高通骁龙865上可以无压力的实时降噪！
+
+---
+Only supports 48kHz audio; the inference code is quite rough, but it works! If you have experience, you'll notice that it is a streaming model, so it can perform real-time denoising! Based on my tests, it can handle real-time denoising effortlessly on a Qualcomm Snapdragon 865!
+
+
+---
+感谢：
+
+https://github.com/Rikorose/DeepFilterNet
+
+https://github.com/grazder/DeepFilterNet/tree/torchDF-changes
+
+```
+@inproceedings{schroeter2022deepfilternet,
+  title={{DeepFilterNet}: A Low Complexity Speech Enhancement Framework for Full-Band Audio based on Deep Filtering}, 
+  author = {Schröter, Hendrik and Escalante-B., Alberto N. and Rosenkranz, Tobias and Maier, Andreas},
+  booktitle={ICASSP 2022 IEEE International Conference on Acoustics, Speech and Signal Processing (ICASSP)},
+  year={2022},
+  organization={IEEE}
+}
---
+++ onnx_infer.py
@@ -0,0 +1,76 @@
+import numpy as np
+import onnxruntime as ort
+import torchaudio
+import torch
+from torch.nn import functional as F
+
+
+#------------------------------init model
+sess_options = ort.SessionOptions()
+sess_options.graph_optimization_level = (
+    ort.GraphOptimizationLevel.ORT_ENABLE_EXTENDED
+)
+sess_options.intra_op_num_threads = 1
+sess_options.inter_op_num_threads = 1
+sess_options.execution_mode = ort.ExecutionMode.ORT_SEQUENTIAL
+
+
+ort_session = ort.InferenceSession(
+    "./denoiser_model.onnx",
+    sess_options,
+    providers=["CPUExecutionProvider"],
+)
+
+input_names = ["input_frame", "states", "atten_lim_db"]
+output_names = ["enhanced_audio_frame", "new_states", "lsnr"]
+
+
+
+
+#------------------------------load wav
+hop_size = 480
+fft_size = 960
+input_audio, sr = torchaudio.load('./examples/A1CIM28ZUCA8RX_M_Street_Near_Regular_SP_Mobile_Primary.wav', channels_first=True)
+input_audio = input_audio.mean(dim=0).unsqueeze(0)  # stereo to mono
+
+input_audio = input_audio.squeeze(0)
+orig_len = input_audio.shape[0]
+
+# padding taken from
+# https://github.com/Rikorose/DeepFilterNet/blob/fa926662facea33657c255fd1f3a083ddc696220/DeepFilterNet/df/enhance.py#L229
+hop_size_divisible_padding_size = (hop_size - orig_len % hop_size) % hop_size
+
+orig_len += hop_size_divisible_padding_size
+input_audio = F.pad(
+    input_audio, (0, fft_size + hop_size_divisible_padding_size)
+)
+
+chunked_audio = torch.split(input_audio, hop_size)
+
+
+#-------------------------------inference
+state = np.zeros(45304,dtype=np.float32)
+atten_lim_db = np.zeros(1,dtype=np.float32)
+enhanced = []
+for frame in chunked_audio:
+    out = ort_session.run(None,input_feed={"input_frame":frame.numpy(),"states":state,"atten_lim_db":atten_lim_db})
+    enhanced.append(torch.tensor(out[0]))
+    state = out[1]
+
+
+
+#-------------------------------save
+enhanced_audio = torch.cat(enhanced).unsqueeze(
+    0
+)  # [t] -> [1, t] typical mono format
+
+d = fft_size - hop_size
+enhanced_audio = enhanced_audio[:, d: orig_len + d]
+
+torchaudio.save(
+    "out.wav",
+    enhanced_audio,
+    sr,
+    encoding="PCM_S",
+    bits_per_sample=16,
+)
